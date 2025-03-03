0938b3c011cb9b9260eeecda4cc18df0b3916714
seantrgyt <94722625+seantrgyt@users.noreply.github.com>
2025-03-02 19:13:29 -0500

    among us jankily fixed

--- public/assets/data/games.js
+++ public/assets/data/games.js
@@ -375,7 +375,7 @@ var games = [
   {
     id: 'amongus',
     title: 'Among Us',
-    url: 'https://assets.3kh0.net/among-us/index.html',
+    url: 'https://amongusonline.io/among-us-online.embed',
     image: 'https://play-lh.googleusercontent.com/8ddL1kuoNUB5vUvgDVjYY3_6HwQcrg1K2fd_R8soD-e2QYj8fT9cfhfh3G0hnSruLKec'
   },
   {
