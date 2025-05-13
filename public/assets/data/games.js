79306707b8ac2fe9ea9486ca20720d272d369886
seantrgyt <94722625+seantrgyt@users.noreply.github.com>
2025-05-13 16:54:51 -0400

    crazy cattle maybe fixed?

--- public/assets/data/games.js
+++ public/assets/data/games.js
@@ -493,7 +493,7 @@ var games = [
   {
     id: 'crazycattle',
     title: 'Crazy Cattle 3D',
-    url: 'https://crazy-cattle3d.org/game/crazycattle3d/v11/',
+    url: 'https://crazycattle3d.io/game/crazycattle3d/v11/',
     image: 'https://img.itch.zone/aW1nLzIwNjg0MzI4LnBuZw==/original/vEMX1y.png'
   }
 ]
